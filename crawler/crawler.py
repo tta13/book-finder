d4f7ff8bcc76daab03674a7bb0e6ca2208cf9cff
Amanda Moraes <amandascm23@gmail.com>
2022-08-10 13:55:32 -0300

    Update crawler webdriver to add user agent

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -49,6 +49,9 @@ class Crawler:
         options.add_argument('--headless')
         options.add_argument("--window-size=1920,1200")
         options.add_argument('--log-level=1')
+        options.add_argument('--user-agent='+"Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
+                                +"AppleWebKit/537.36 (KHTML, like Gecko)"
+                                +"Chrome/87.0.4280.141 Safari/537.36")
         return webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
 
     def download_url(self, url):
