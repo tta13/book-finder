3cb3033cc5207039839ce80d6edb011ed123270e
Amanda Moraes <amandascm23@gmail.com>
2022-08-10 22:34:32 -0300

    Remove unnecessary 're' dependency

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -5,7 +5,6 @@ from urllib.parse import urljoin, urlparse
 from bs4 import BeautifulSoup
 import os
 from dotenv import load_dotenv
-import re
 import time
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
