9df90aa9e20993792c1eaf8207f5aec787c36163
Amanda Moraes <amandascm23@gmail.com>
2022-08-20 23:41:08 -0300

    Update HtmlClassifier to run on multiple subdirs + prepare wrapper input

--- classifier/HTMLClassifier/HTMLclassifier.py
+++ classifier/HTMLClassifier/HTMLclassifier.py
@@ -10,6 +10,7 @@ from sklearn import metrics
 import joblib
 import numpy as np
 import os
+import shutil
 
 class HTML_classifier():
     def __init__(self) -> None:
@@ -29,5 +30,30 @@ class HTML_classifier():
 
         #x = [self.preprocess_url(HTML)]
         y = self.saved_clf.predict(souper) # format: [[prob_0, prob_1]]
-        print(y)
         return (y) # returns prob of being a book page url
+
+    def predict_list_html_hr(self, html_path_list):
+        responses = []
+        for html_path in html_path_list:
+            responses += self.predict_score(html_path)
+        harvest_ratio = sum(responses) / len(responses)
+        return harvest_ratio
+    
+    def predict_subdirectory_hr(self, subdirectory):
+        html_path_list = [os.path.join(subdirectory, f) for f in os.listdir(subdirectory)]
+        results = self.predict_score(html_path_list)
+        harvest_ratio = sum(results)/len(results)
+        self.prepare_html_for_wrapper(subdirectory, html_path_list, results)
+        return harvest_ratio
+    
+    def prepare_html_for_wrapper(self, subdirectory, html_path_list, results):
+        dominio = subdirectory.split('-')[0]
+        strategy = subdirectory.split('-')[1]
+        destin_dir = os.path.join('..', '..', 'data', f'positive-{strategy}', dominio)
+        html_file_list = os.listdir(subdirectory)
+        if not os.path.exists(destin_dir):
+            os.makedirs(destin_dir)
+        for i in range(len(results)):
+            destin_file = os.path.join(destin_dir, html_file_list[i])
+            if results[i] == 1:
+                shutil.copy(html_path_list[i], destin_file)
